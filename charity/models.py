6edce5fb95d144185637fe525e09c2b53a2db5d0
agnieszka <agnieszka.tylenda@gmail.com>
2020-09-27 00:16:37 +0200

    sekcja 1

--- charity/models.py
+++ charity/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=128)
+
+
+class Institution(models.Model):
+    FUNDACJA = 0
+    ORGANIZACJA_POZARZADOWA = 1
+    ZBIORKA_LOKALNA = 2
+
+    TYPE = {
+    (FUNDACJA, 'Fundacja'),
+    (ORGANIZACJA_POZARZADOWA, 'Organizacja Pozarządowa'),
+    (ZBIORKA_LOKALNA, 'Zbiórka lokalna'),
+}
+
+    name = models.CharField(max_length=128)
+    description = models.TextField(null=True)
+    type = models.IntegerField(choices=TYPE, default=FUNDACJA)
+    categories = models.ManyToManyField("Category")
+
+
+class Donation(models.Model):
+    quantity = models.SmallIntegerField()
+    categories = models.ManyToManyField("Category")
+    institution = models.ForeignKey("Institution", on_delete=models.CASCADE)
+    address = models.CharField(max_length=256)
+    phone_number = models.SmallIntegerField()
+    city = models.CharField(max_length=256)
+    zip_code = models.CharField(max_length=6)
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.TextField(null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, default=None)
