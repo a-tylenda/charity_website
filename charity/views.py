29d5bc84957a58b1358d7dfd44920b9e8c3381f1
agnieszka <agnieszka.tylenda@gmail.com>
2020-09-26 23:06:46 +0200

    sekcja 0

--- charity/views.py
+++ charity/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views import View
 
-# Create your views here.
+
+class LandingPage(View):
+    def get(self, request):
+        ctx = {}
+        return render(request, 'index.html', ctx)
+
+
+class AddDonation(View):
+    def get(self, request):
+        ctx = {}
+        return render(request, 'form.html', ctx)
+
+
+class Login(View):
+    def get(self, request):
+        ctx = {}
+        return render(request, 'login.html', ctx)
+
+
+class Register(View):
+    def get(self, request):
+        ctx = {}
+        return render(request, 'register.html', ctx)
