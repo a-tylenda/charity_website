8417acc31053505db05b6403ebf3c5f3d9dea54b
agnieszka <agnieszka.tylenda@gmail.com>
2020-09-27 20:12:33 +0200

    sekcja #2

--- charity/models.py
+++ charity/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=128)
 
+    def __str__(self):
+        return f"{self.name}"
 
 class Institution(models.Model):
     FUNDACJA = 0
@@ -21,16 +23,21 @@ class Institution(models.Model):
     type = models.IntegerField(choices=TYPE, default=FUNDACJA)
     categories = models.ManyToManyField("Category")
 
+    def __str__(self):
+        return f"{self.name}"
 
 class Donation(models.Model):
     quantity = models.SmallIntegerField()
     categories = models.ManyToManyField("Category")
     institution = models.ForeignKey("Institution", on_delete=models.CASCADE)
     address = models.CharField(max_length=256)
-    phone_number = models.SmallIntegerField()
+    phone_number = models.IntegerField()
     city = models.CharField(max_length=256)
     zip_code = models.CharField(max_length=6)
     pick_up_date = models.DateField()
     pick_up_time = models.TimeField()
     pick_up_comment = models.TextField(null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, default=None)
+
+    def __str__(self):
+        return f"{self.user.username} {self.categories.name} {self.quantity}"
--- charity/views.py
+++ charity/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
+from .models import Category, Institution, Donation
 
 
 class LandingPage(View):
     def get(self, request):
-        ctx = {}
+        donations = Donation.objects.all().count()
+        institutions = Institution.objects.all().count()
+        foundations = Institution.objects.filter(type=Institution.FUNDACJA)
+        organizations = Institution.objects.filter(type=Institution.ORGANIZACJA_POZARZADOWA)
+        local_collections = Institution.objects.filter(type=Institution.ZBIORKA_LOKALNA)
+        ctx = {
+            "donations": donations,
+            "institutions": institutions,
+            "foundations": foundations,
+            "organizations": organizations,
+            "local_collections": local_collections
+        }
         return render(request, 'index.html', ctx)
 
 
